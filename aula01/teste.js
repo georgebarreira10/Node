e4bfc6e14ced6298048cc01eb4a90fbc28914137
george barreira <barreirageorge@gmail.com>
2022-09-29 19:46:08 -0300

    setInterval de 1000(1s) e calculo da porcentagem da memoria utilizada

--- aula01/teste.js
+++ aula01/teste.js
@@ -5,14 +5,19 @@ const os = require('os')
 
 //  o modulo 'os' está dentro da variavel 'os', desta forma conseguimos
 // acessar as propriedades e metodos deste modulo/objeto.
+setInterval(() => {
 
-const totalMem = parseInt(os.totalmem() / 1024 / 1024)
-const freeMem = parseInt(os.freemem() / 1024 / 1024)
-const memoria = {
-    total: `${totalMem} MB`,
-    free: `${freeMem} MB`,
-}
-console.clear()
-console.table(memoria)
+    const totalMem = parseInt(os.totalmem() / 1024 / 1024)
+    const freeMem = parseInt(os.freemem() / 1024 / 1024)
+    const percent = parseInt((freeMem / totalMem) * 100)
+    
+    const memoria = {
+        total: `${totalMem} MB`,
+        free: `${freeMem} MB`,
+        percent: `${percent} %`
+    }
+    console.clear()
+    console.table(memoria)
+}, 1000)
 
 //console.log(`Memória Livre: ${ freeMem }`, `Total de memória: ${ totalMem } `)
